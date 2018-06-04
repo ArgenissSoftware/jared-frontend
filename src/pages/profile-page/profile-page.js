a6a3e7709ac31771f6b2ec7355836ad078cc26b1
Samuel Garcia <fnc.samuel.garcia@gmail.com>
2018-06-04 11:31:32 -0300

    add message success

--- src/pages/profile-page/profile-page.js
+++ src/pages/profile-page/profile-page.js
@@ -8,11 +8,21 @@ import AppStore from "../../stores/AppStore";
 import ProfileStore from "../../stores/ProfileStore";
 import TopNavBar from "../../components/TopNavBar/top-nav-bar";
 
+import signUpStore from "../../stores/SignUpStore";
+import { Redirect } from "react-router-dom";
+
+let updateSuccessMessage = false;
+
 const ProfilePage = observer(
   class ProfilePage extends Component {
     constructor(props) {
       super(props);
       this.save = this.save.bind(this);
+      signUpStore.navigate = false;
+    }
+
+    handleChange(e) {
+      signUpStore[e.target.name] = e.target.value;
     }
 
     async save() {
@@ -43,6 +53,8 @@ const ProfilePage = observer(
         })
         .then(
           function(response) {
+            updateSuccessMessage = true;
+            signUpStore.navigate = true;
             this.props.history.push("home");
           }.bind(this)
         )
@@ -52,6 +64,9 @@ const ProfilePage = observer(
     }
 
     render() {
+      if (signUpStore.navigate){
+        return <Redirect to={{ pathname: "/home", state: { updateSuccessMessage: updateSuccessMessage } }} push={true} />;
+      }
       return (
         <div>
           <TopNavBar history={this.props.history} />
