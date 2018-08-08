7e4909534935189b8ee46a5429c47f154f0b5ec0
Yaher <colladofacundo@hotmail.com>
2018-08-08 00:27:49 -0300

    Added delete client ajax method and confirmation modal

--- src/pages/clients-page/clients-page.js
+++ src/pages/clients-page/clients-page.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import { observer } from "mobx-react";
 import "./clients-page.css";
-import { Button, Input, List } from "semantic-ui-react";
+import { Button, Input, List, Modal } from "semantic-ui-react";
 import axios from "axios";
 import ClientsStore from "../../stores/ClientsStore";
 import AppStore from "../../stores/AppStore";
 import TopNavBar from "../../components/TopNavBar/top-nav-bar";
 import authStore from "../../stores/AuthStore";
+
+let delClientName = "";
+let delClientId = "";
 
 const ClientsPage = observer(
   class ClientsPage extends Component {
@@ -14,9 +17,15 @@ const ClientsPage = observer(
       super(props);
       ClientsStore.getClientsList();
       this.addClient = this.addClient.bind(this);
+      this.removeClient = this.removeClient.bind(this);
+      this.openConfirmationModal = this.openConfirmationModal.bind(this);
     }
 
-    getRenderedClientsList(clientName) {
+    state = { modalOpen: false }
+    handleOpen = () => this.setState({ modalOpen: true })
+    handleClose = () => this.setState({ modalOpen: false })
+
+    getRenderedClientsList(clientName, clientId) {
       return (
         <List.Item key={clientName}>
           <List.Icon name="user" size="large" verticalAlign="middle" />
@@ -24,6 +33,8 @@ const ClientsPage = observer(
             <List.Header as="a">{clientName}</List.Header>
             <List.Description as="a">Project Description</List.Description>
           </List.Content>
+          <List.Icon className="times icon removeClientIcon" size="large" verticalAlign="middle"
+            onClick={() => this.openConfirmationModal(clientName, clientId)} />
         </List.Item>
       );
     }
@@ -40,18 +51,44 @@ const ClientsPage = observer(
               headers: {
                 Authorization: "Bearer " + authStore.token,
                 "Content-Type": "application/json"
-                
+
               }
             }
           )
-          .then(function(response) {
+          .then(function (response) {
             ClientsStore.getClientsList();
           })
-          .catch(function(error) {
+          .catch(function (error) {
             console.log(error);
           });
       }
       ClientsStore.newClientsInput = "";
+    }
+
+    openConfirmationModal(clientName, clientId) {
+      delClientId = clientId;
+      delClientName = clientName;
+      this.setState({ modalOpen: true });
+    }
+
+    async removeClient() {
+      await axios
+        .delete(
+          AppStore.URL + "/clients/" + delClientId,
+          {
+            headers: {
+              Authorization: "Bearer " + authStore.token,
+              "Content-Type": "application/json"
+            }
+          }
+        )
+        .then(function (response) {
+          ClientsStore.getClientsList();
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+        this.setState({ modalOpen: false });
     }
 
     handleMessage(e) {
@@ -71,10 +108,21 @@ const ClientsPage = observer(
             />
             <List divided relaxed>
               {ClientsStore.clients.map(client =>
-                this.getRenderedClientsList(client.name)
+                this.getRenderedClientsList(client.name, client._id)
               )}
             </List>
           </div>
+
+          <Modal open={this.state.modalOpen} onClose={this.handleClose} >
+            <Modal.Header>Confirmation needed</Modal.Header>
+            <Modal.Content>
+              <p>Are you sure you want to remove {delClientName} from your clients list ?</p>
+            </Modal.Content>
+            <Modal.Actions>
+              <Button onClick={this.handleClose} negative>No</Button>
+              <Button color='green' onClick={() => this.removeClient()} >Yes</Button>
+            </Modal.Actions>
+          </Modal>
         </div>
       );
     }
