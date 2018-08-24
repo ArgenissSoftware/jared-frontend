d847a65ce1068f0adb29c715aef52b3e09d01073
alejandro_negri <alejandro.negri@argeniss.com>
2018-08-24 13:37:38 -0300

    moved side menu to top bar

--- src/components/TopNavBar/top-nav-bar.js
+++ src/components/TopNavBar/top-nav-bar.js
@@ -1,43 +1,65 @@
-import React, { Component } from "react";
-import { observer } from "mobx-react";
+import React, {Component} from "react";
+import {observer} from "mobx-react";
 import logo from "../../images/logo1.png";
 import UserStore from "../../stores/UserStore";
-import { Dropdown, Menu, Icon } from "semantic-ui-react";
+import {Dropdown, Menu, Icon} from "semantic-ui-react";
 import "./top-nav-bar.css";
 
-const TopNavBar = observer(
-  class TopNavBar extends Component {
-    handleItemClick = (e, { name }) => this.props.history.push(name);
+const TopNavBar = observer(class TopNavBar extends Component {
+  handleItemClick = (e, {name}) => this
+    .props
+    .history
+    .push(name);
 
-    render() {
-      return (
-        <Menu size="huge">
-          <Menu.Item name="../home" onClick={this.handleItemClick}>
-            <img src={logo} alt="" />
-            <b>Jared</b>
-          </Menu.Item>
-          <Menu.Menu position="right">
-            <Dropdown item text={"Welcome " + UserStore.user.username}>
-              <Dropdown.Menu>
-                <Dropdown.Item name="../home/profile" onClick={this.handleItemClick}>
-                <Icon name='user' />
-                  My Profile
-                </Dropdown.Item>
-                <Dropdown.Item name="../home/change_password" onClick={this.handleItemClick}>
-                <Icon name='lock' />
-                  Change Password
-                </Dropdown.Item>
-                <Dropdown.Item name="/" onClick={this.handleItemClick}>
-                <Icon name='sign out' />
-                  Logout
-                </Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Menu.Menu>
-        </Menu>
-      );
-    }
+  render() {
+    return (
+      <Menu >
+        <Menu.Item name="../home" onClick={this.handleItemClick}>
+          <img src={logo} alt=""/>
+          <b>Jared</b>
+        </Menu.Item>
+
+        <Dropdown item text="User">
+          <Dropdown.Menu>
+            <Dropdown.Item name="../home/users" onClick={this.handleItemClick}>
+              <Icon name='list'/>
+              User List
+            </Dropdown.Item>
+
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <Dropdown item text="Client">
+          <Dropdown.Menu>
+            <Dropdown.Item name="../home/clients" onClick={this.handleItemClick}>
+              <Icon name='list'/>
+              Client List
+            </Dropdown.Item>
+
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <Menu.Menu position="right">
+          <Dropdown item text={"Welcome " + UserStore.user.username}>
+            <Dropdown.Menu>
+              <Dropdown.Item name="../home/profile" onClick={this.handleItemClick}>
+                <Icon name='user'/>
+                My Profile
+              </Dropdown.Item>
+              <Dropdown.Item name="../home/change_password" onClick={this.handleItemClick}>
+                <Icon name='lock'/>
+                Change Password
+              </Dropdown.Item>
+              <Dropdown.Item name="/" onClick={this.handleItemClick}>
+                <Icon name='sign out'/>
+                Logout
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Menu.Menu>
+      </Menu>
+    );
   }
-);
+});
 
 export default TopNavBar;
