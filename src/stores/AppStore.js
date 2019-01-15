8f12d9e8c7a2781cd0ffc0e1e79138a41dc5bb24
Nahuel Herrlein <herrlein.n@gmail.com>
2019-01-15 11:56:53 -0300

    Fix the URL

--- src/stores/AppStore.js
+++ src/stores/AppStore.js
@@ -1,7 +1,7 @@
 
 class AppStore {
   constructor() {
-    this.URL = process.env.REACT_APP_API_HOST || "http://192.168.99.100:3000/api"
+    this.URL = process.env.REACT_APP_API_HOST || "http://localhost:3000/api"
   }
 }
 
