3f157e58bc447dacb21b94fdbf6e03117f80fdd5
dmunozlarrosa <davidmml1988@gmail.com>
2019-01-24 07:16:41 -0300

    feature/98 Create a client component for the lists

---
+++ src/components/ClientCard/clientCard.component.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Feed, Icon, Button, Divider } from 'semantic-ui-react'
+
+class ClientCard extends Component {
+
+  getDeleteClient() {
+    if (this.props.deleteClient) {
+      return (
+        <Feed.Content floated='right' >
+          <Button circular icon='delete' onClick={() => this.props.deleteClient(this.props._id)}></Button>
+        </Feed.Content>
+      )
+    } else {
+      return null
+    }
+  }
+
+  getNameAndDescription() {
+    return (
+      <div>
+        <Feed.Summary>
+          <a>{this.props.name}</a>
+        </Feed.Summary>
+        <Feed.Extra text >
+          Project Description
+        </Feed.Extra>
+      </div>
+    )
+  }
+
+  getContent() {
+    if (this.props.history) {
+      return (
+        <Feed.Content onClick={() => this.goToDetail(this.props._id)}>
+          {this.getNameAndDescription()}
+        </Feed.Content>
+      )
+    } else {
+      return (
+        <Feed.Content>
+          {this.getNameAndDescription()}
+        </Feed.Content>
+      )
+    }
+  }
+
+  goToDetail(id) {
+    this.props.history.push("clients/" + id);
+  }
+
+  render() {
+    return (
+      <Feed>
+        <Feed.Event>
+          <Icon name='user' size='big' />
+          {this.getContent()}
+          {this.getDeleteClient()}
+        </Feed.Event>
+        <Divider/>
+      </Feed>
+    );
+  }
+}
+
+export default ClientCard;
