8e1a764e37c711ba78f922dcfc1d34068edf4d90
Martin Santangelo <msantang78@gmail.com>
2020-08-30 19:57:07 -0300

    wip

--- src/stores/CreateRelationStore.js
+++ src/stores/CreateRelationStore.js
@@ -15,11 +15,11 @@ export function createRelationStore(store, url, fieldName) {
     },
     async add() {
       if (this.selected) {
-        if (store.entity[fieldName].find((relatedEntity) => relatedEntity._id === this.selected._id)) {
-          store.setError('The item is already in the list');
-        } else {
+        // if (store.entity[fieldName].find((relatedEntity) => relatedEntity._id === this.selected._id)) {
+        //   store.setError('The item is already in the list');
+        // } else {
           await store.addRelation(this.selected, url, fieldName);
-        }
+        // }
       }
     }
   });
